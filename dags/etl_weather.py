61bff4d2223855d84cbf0c02ef738ae75ce16e6c
NLdouglas <douglasleodoro.qi@gmail.com>
2025-06-18 17:56:08 -0300

    etl complete, initializing docker compose

--- dags/etl_weather.py
+++ dags/etl_weather.py
@@ -47,8 +47,54 @@ with DAG(dag_id='weather_etl_pipeline',
     def transform_weather_data(weather_data) :
         current_weather = weather_data['current_weather']
         transform_data = {
-            
+            'latitude': LATITUDE,
+            'longitude': LONGITUDE,
+            'temperature': current_weather['temperature'],
+            'windspeed': current_weather['windspeed'],
+            'winddirection': current_weather['winddirection'],
+            'weathercode': current_weather['weathercode'],
         }
+        return transform_data
+    
+
+    #load transformed data into PostgreSQL
+    @task()
+    def load_weather_data(transformed_data):
+        pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
+        conn = pg_hook.get_conn()
+        cursor = conn.cursor()
+
+        #create table if not exists
+        cursor.execute(""""
+            CREATE TABLE IF NOT EXISTS weather_data (
+                latitude FLOAT NOT NULL,
+                longitude FLOAT NOT NULL,
+                temperature FLOAT NOT NULL,
+                windspeed FLOAT NOT NULL,
+                winddirection INT NOT NULL,
+                weathercode INT NOT NULL,
+                timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+        """)
+
+        #insert data into table
+        cursor.execute("""
+            INSERT INT weather_data ( latitude, longitude, temperature, windspeed, winddirection, weathercode)
+            VALUES (%s, %s, %s, %s, %s, %s)""", (
+                transformed_data['latitude'],
+                transformed_data['longitude'],
+                transformed_data['temperature'],
+                transformed_data['windspeed'],
+                transformed_data['winddirection'],
+                transformed_data['weathercode']
+            ))
+        conn.commit()
+        cursor.close()       
+
+    # DAG workflow 
+    weather_data = extract_weather_data()
+    transformed_data = transform_weather_data(weather_data)
+    load_weather_data(transformed_data)
 
 
     
